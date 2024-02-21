201ebe8cb5f87d85c52df5d0c3f62f6fda36eae4
Chapapa <48244364+Chapapa78@users.noreply.github.com>
2024-02-21 17:12:56 +0100

    inutile

--- ApplicationLog/vue/SQL.php
+++ ApplicationLog/vue/SQL.php
@@ -16,16 +16,15 @@ $title = 'Serveur Web';
 
 ob_start();
 
-$fichier=fopen("/var/log/apache2/error.log", "r"); //r : read, w : write, a : append
+$fichier=fopen("/var/log/apache2/access.log", "r"); //r : read, w : write, a : append
 if ($fichier==null){
 echo"<br> Attention, fichier logs introuvable <br>";}
 
 $fichier2=file_exists("/var/log/apache2/access.log"); //r : read, w : write, a : append
 
-if($fichier2==true){ echo "OK fichier";}
+if($fichier2==true){ echo "<br>OK fichier<br>";}
 ?>
-TEST SQL
-ICI faire traitement des log récup dans le serveur SQL c'est à dire les inserer dans la base de donnée ici de facon invisible
+
 <a class="nav-link js-scroll-trigger" href="index.php?page=sqllog">LOG SQL</a>
 <a class="nav-link js-scroll-trigger" href="index.php?page=sqlstats">Statistique SQL</a><?php
 $contenu = ob_get_clean();
