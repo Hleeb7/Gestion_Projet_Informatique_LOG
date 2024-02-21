e77408f8b913b8c297cb466af65f4810d9d47689
vvvtttxxx <160126727+vvvtttxxx@users.noreply.github.com>
2024-02-21 12:20:53 +0100

    Add files via upload

--- ApplicationLog/vue/SQL.php
+++ ApplicationLog/vue/SQL.php
@@ -7,28 +7,23 @@
  */
 
 $title = 'Serveur Web';
-
-
-
-
-
-
-
 ob_start();
-
-$fichier=fopen("/var/log/apache2/error.log", "r"); //r : read, w : write, a : append
-if ($fichier==null){
-echo"<br> Attention, fichier logs introuvable <br>";}
-
-$fichier2=file_exists("/var/log/apache2/access.log"); //r : read, w : write, a : append
-
-if($fichier2==true){ echo "OK fichier";}
 ?>
-TEST SQL
-ICI faire traitement des log récup dans le serveur SQL c'est à dire les inserer dans la base de donnée ici de facon invisible
-<a class="nav-link js-scroll-trigger" href="index.php?page=sqllog">LOG SQL</a>
-<a class="nav-link js-scroll-trigger" href="index.php?page=sqlstats">Statistique SQL</a><?php
-$contenu = ob_get_clean();
-render($contenu,  $title);
+<div style="text-align: center;">
+    <br/>
+    <br/>
+    <br/>
+    <br/>
+    <br/>
 
+    TEST SQL
+    <br/>
+    ICI faire traitement des logs récupérés dans le serveur SQL, c'est à dire les insérer dans la base de données ici de façon invisible.
+    <br/>
+    <a class="nav-link js-scroll-trigger" href="index.php?page=sqllog">LOG SQL</a>
+    <a class="nav-link js-scroll-trigger" href="index.php?page=sqlstats">Statistique SQL</a>
+</div>
+<?php
+$contenu = ob_get_clean();
+render($contenu, $title);
 ?>
